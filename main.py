890a06fb8dbfe98703dd3b95ef5b43040cc18573
Joshua Whiting <j.whiting-2018@hull.ac.uk>
2019-03-18 14:20:09 +0000

    Updating files

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import cv2 as cv
 import numpy as np
 from options import Options
+import os.path
 import sys
 
 
 def main():
-
     # Options
-    options = Options().get_options()
+    options = Options().options
 
     # Files
-    image_file = "woody//00001.png"
-    template_file = "template//woody1.png"
+    image_file = os.path.join(options.input_folder, "00001.png")
 
     # Load the images
     image = cv.imread(image_file)
@@ -21,56 +20,87 @@ def main():
     image_gray = cv.equalizeHist(image_gray)
 
     if options.mode == "cascade":
+
         # Load the cascade file
-        cascade_file = cv.CascadeClassifier("cascade//haarcascade_frontalface_default.xml")
+        cascade_file = cv.CascadeClassifier(options.cascade_file)
+        eye_file = cv.CascadeClassifier("cascade//haarcascade_eye.xml")
 
         # Detect faces in the image
         rects = cascade_file.detectMultiScale(
             image_gray,
             scaleFactor=1.1,
             minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv.CV_HAAR_SCALE_IMAGE
+            minSize=(40, 40),
+            flags=cv.CASCADE_SCALE_IMAGE
         )
-
-
-        # TODO:
-        rects[:, 2:] += rects[:, :2]
 
         # Draw rectangles over the matches
         for (x, y, w, h) in rects:
             cv.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    elif options.mode == "template":
-        # Load the template file
+        """TEST"""
+        # Detect faces in the image
+        rects2 = eye_file.detectMultiScale(
+            image_gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv.CASCADE_SCALE_IMAGE
+        )
 
-        template = cv.imread(template_file)
+        # Draw rectangles over the matches
+        for (x, y, w, h) in rects2:
+            cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 255), 2)
+
+    elif options.mode == "template":
+
+        # Load the template file
+        template = cv.imread(options.template_file)
 
         # Convert to grayscale
         template_gray = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
+        template_gray = cv.equalizeHist(template_gray)
+        template_width, template_height = template_gray.shape[::-1]
 
-        # Get template height and width
-        w, h = template_gray.shape[::-1]
+        for i in range(options.zoom_steps):
+            for flip in [True, False]:
+                scaling_factor = 1 - (i / options.zoom_steps)
 
-        # Apply template Matching
-        res = cv.matchTemplate(image_gray, template_gray, cv.TM_CCOEFF_NORMED)
-        threshold = 0.4
-        print(np.max(res))
+                if template_width * scaling_factor < options.output_size:
+                    break;
 
-        """if np.any(res >= threshold):
-            _, _, _, max_loc = cv2.minMaxLoc(res)
-    
-            top_left = max_loc
-            bottom_right = (top_left[0] + w, top_left[1] + h)
-    
-            cv2.rectangle(image, top_left, bottom_right, (255, 0, 0), 2)"""
+                print()
+                print("Scaling Factor: " + str(scaling_factor))
 
-        # Filtering results to examples over the threshold
-        location = np.where(res >= threshold)
+                template_scaled = cv.resize(template_gray.copy(), (0, 0), fx=scaling_factor, fy=scaling_factor, interpolation=cv.INTER_AREA)
 
-        # Draw rectangles over the matches
-        for x, y in zip(*location[::-1]):
-            cv.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                # Flip the template image vertically
+                print("Template Flipped: " + str(flip))
+                if flip:
+                    template_scaled = cv.flip(template_scaled, 1)
+
+                # Get template height and width
+                w, h = template_scaled.shape[::-1]
+
+                # Apply template Matching
+                res = cv.matchTemplate(image_gray, template_scaled, cv.TM_CCOEFF_NORMED)
+                threshold = 0.5
+                print(np.max(res))
+
+                """if np.any(res >= threshold):
+                    _, _, _, max_loc = cv.minMaxLoc(res)
+            
+                    top_left = max_loc
+                    bottom_right = (top_left[0] + w, top_left[1] + h)
+            
+                    cv.rectangle(image, top_left, bottom_right, (255, 0, 0), 2)"""
+
+                # Filtering results to examples over the threshold
+                location = np.where(res >= threshold)
+
+                # Draw rectangles over the matches
+                for x, y in zip(*location[::-1]):
+                    cv.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     else:
         print("Incorrect mode selected")
--- options.py
+++ options.py
@@ -5,17 +5,30 @@ class Options:
     def __init__(self):
         self.parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    def get_options(self):
-        self.parser.add_argument('--input_folder', type=str, default='.', help='path to images')
-        self.parser.add_argument('--mode', type=str, default='cascade',
-                                 help='method to find faces. [cascade | template]')
+        self.parser.add_argument("--input_folder", type=str, default="woody//", help="path to images")
+        self.parser.add_argument("--output_size", type=int, default=256, help="size in pixels of the output image")
 
-        self.opt = self.parser.parse_args()
-        args = vars(self.opt)
+        self.parser.add_argument("--mode", type=str, default="cascade",
+                                 help="method to find faces. [cascade | template]")
+
+        # Cascade options
+        self.parser.add_argument("--cascade_file", type=str, default="cascade//haarcascade_frontalface_default.xml",
+                                 help="filepath to the cascade file")
+
+        # Template options
+        self.parser.add_argument("--template_file", type=str, default="template//woody1.png",
+                                 help="filepath to the template file")
+        self.parser.add_argument("--zoom-steps", type=int, default=20, help="Number of times to resize the template image")
+
+        self.options = self.parser.parse_args()
+
+        # Print the option values to the console
+        self.print()
+
+    def print(self):
+        args = vars(self.options)
 
         print('------------ Options -------------')
         for k, v in sorted(args.items()):
             print('%s: %s' % (str(k), str(v)))
         print('-------------- End ----------------')
-
-        return self.opt
