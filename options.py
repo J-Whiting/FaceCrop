13485980d6555e043705788c727c723e9e158f83
Joshua Whiting <J.Whiting-2018@hull.ac.uk>
2019-04-04 10:58:28 +0100

    Update options.py
    Making the options generic

--- options.py
+++ options.py
@@ -5,8 +5,8 @@ class Options:
     def __init__(self):
         self.parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-        self.parser.add_argument("--video_file", type=str, default=r"toy-story.mp4", help="filename of video file")
-        self.parser.add_argument("--classifier_file", type=str, default=r"classifier/woody.xml",
+        self.parser.add_argument("--video_file", type=str, default=r"", help="filename of video file")
+        self.parser.add_argument("--classifier_file", type=str, default=r"classifier/haarcascade_frontalface_default.xml",
                                  help="filepath to the classifier file")
         self.parser.add_argument("--output_folder", type=str, default=r"output/", help="path to output folder")
         self.parser.add_argument("--output_size", type=int, default=256, help="size in pixels of the output image")
